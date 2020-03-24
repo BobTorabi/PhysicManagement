ac792d07ed921aeb7da1738d24e564a958c1138a
amir <amir.m.s.137979@gmil.com>
2020-03-24 21:51:02 +0430

    Update MedicalRecordService

--- PhysicManagement/PhysicManagement.Logic/Services/MedicalRecordService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/MedicalRecordService.cs
@@ -3,23 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhysicManagement.Logic.Validations;
+using FluentValidation;
 
 namespace PhysicManagement.Logic.Services
 {
-   public class MedicalRecordService
-   {
+    public class MedicalRecordService
+    {
         #region MedicalRecord section
 
         public List<Model.MedicalRecord> GetMedicalRecordList()
-        { }
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                return db.MedicalRecord.OrderBy(x => x.Id).ToList();
+            }
+        }
         public Model.MedicalRecord GetMedicalRecordById(int entityId)
-        { }
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.MedicalRecord.Find(entityId);
+                return Entity;
+            }
+        }
         public bool AddMedicalRecord(Model.MedicalRecord entity)
-        { }
+        {
+            var valition = new MedicalRecordValidation.MedicalRecordEntityValidation().Validate(entity);
+            if (!valition.IsValid)
+                throw new ValidationException(valition.Errors);
+
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                db.MedicalRecord.Add(entity);
+                return db.SaveChanges() == 1;
+            }
+        }
         public bool UpdateMedicalRecord(Model.MedicalRecord entity)
-        { }
+        {
+            var validtion = new MedicalRecordValidation.MedicalRecordEntityValidation().Validate(entity);
+            if (!validtion.IsValid)
+                throw new ValidationException(validtion.Errors);
+
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.MedicalRecord.Find(entity.Id);
+                Entity.PatientFirstName = entity.PatientFirstName;
+                Entity.PatientLastName = entity.PatientLastName;
+                Entity.DoctorFirstName = entity.DoctorFirstName;
+                Entity.DoctorLastName = entity.DoctorLastName;
+                Entity.CTCode = entity.CTCode;
+                Entity.CTDescription = entity.CTDescription;
+                Entity.TPCode = entity.TPCode;
+                Entity.TPDescription = entity.TPDescription;
+                Entity.MRICode = entity.MRICode;
+                Entity.CancerTitle = entity.CancerTitle;
+
+                return db.SaveChanges() == 1;
+            }
+        }
         public bool DeleteMedicalRecord(Model.MedicalRecord entityId)
-        { }
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.MedicalRecord.Find(entityId);
+                if (Entity == null)
+                    throw new ValidationException("این رکورد در پایگاه دادهوجود ندارد");
+                db.MedicalRecord.Remove(Entity);
+                return db.SaveChanges() == 1;
+            }
+        }
 
         #endregion
     }
