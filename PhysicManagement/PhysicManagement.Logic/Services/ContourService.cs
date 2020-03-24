15e2b31a54874cd5e386003b6df341c50e666ad8
amir <amir.m.s.137979@gmil.com>
2020-03-24 18:35:32 +0430

    Add Some Method in ContourService
    add CRUD Method For Contour

--- PhysicManagement/PhysicManagement.Logic/Services/ContourService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/ContourService.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace PhysicManagement.Logic.Services
 {
-    class ContourService
-    {
-        // Contour CRUD needed
-        // ContourDetail CRUD needed
+   public class ContourService
+   {
+        #region Contour section
+
+        public List<Model.Contour> GetContourList()
+        { }
+        public Model.Contour GetContourById(int entityId)
+        { }
+        public bool AddContour(Model.Contour entity)
+        { }
+        public bool UpdateContour(Model.Contour entity)
+        { }
+        public bool DeleteContour(Model.Contour entityId)
+        { }
+        #endregion
+
+        #region ContourDetail section
+
+        #endregion
+
 
     }
 }
