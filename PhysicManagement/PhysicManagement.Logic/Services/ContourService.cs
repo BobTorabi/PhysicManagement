207dcd925d0da1e41aba19ace584efeaa2203431
amir <amir.m.s.137979@gmil.com>
2020-03-24 19:51:03 +0430

    Update Contour's Method

--- PhysicManagement/PhysicManagement.Logic/Services/ContourService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/ContourService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhysicManagement.Logic.Validations;
+using FluentValidation;
 
 namespace PhysicManagement.Logic.Services
 {
@@ -11,15 +13,60 @@ namespace PhysicManagement.Logic.Services
         #region Contour section
 
         public List<Model.Contour> GetContourList()
-        { }
+        {
+            using(var db = new Model.PhysicManagementEntities())
+            {
+                return db.Contour.OrderBy(x => x.Id).ToList();
+            }
+        }
         public Model.Contour GetContourById(int entityId)
-        { }
+        {
+            using(var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Contour.Find(entityId);
+                return Entity;
+            }
+        }
         public bool AddContour(Model.Contour entity)
-        { }
+        {
+            var validtion = new ContourValidation.ContourEntityValidate().Validate(entity);
+            if (!validtion.IsValid)
+                throw new ValidationException(validtion.Errors);
+
+            using(var db = new Model.PhysicManagementEntities())
+            {
+                db.Contour.Add(entity);
+                return db.SaveChanges() == 1;
+            }
+        }
         public bool UpdateContour(Model.Contour entity)
-        { }
+        {
+            var validtion = new ContourValidation.ContourEntityValidate().Validate(entity);
+            if (!validtion.IsValid)
+                throw new ValidationException(validtion.Errors);
+
+            using(var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Contour.Find(entity.Id);
+                Entity.Description = entity.Description;
+                Entity.ExtraInfo1 = entity.ExtraInfo1;
+                Entity.ExtraInfo2 = entity.ExtraInfo2;
+
+                return db.SaveChanges() == 1;
+            }
+        }
         public bool DeleteContour(Model.Contour entityId)
-        { }
+        {
+            using(var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Contour.Find(entityId);
+                if (Entity == null)
+                    throw new ValidationException("این رکورد در پایگاه داده وجود ندارد");
+
+                db.Contour.Remove(Entity);
+                return db.SaveChanges() == 1;
+            }
+        }
         #endregion
 
         #region ContourDetail section
