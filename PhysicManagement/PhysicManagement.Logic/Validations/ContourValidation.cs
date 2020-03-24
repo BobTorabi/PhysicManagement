31b078c8f4a2ff5e3ebb4f0a58b2e180c5ce6434
amir <amir.m.s.137979@gmil.com>
2020-03-24 19:07:10 +0430

    Add ContourValidtion

--- PhysicManagement/PhysicManagement.Logic/Validations/ContourValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/ContourValidation.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace PhysicManagement.Logic.Validations
 {
-    class ContourValidation
+    public class ContourValidation
     {
+        public class ContourEntityValidate : AbstractValidator<Model.ContourDetails>
+        {
+            public ContourEntityValidate()
+            {
+                RuleFor(e => e.Id).NotNull().NotEmpty().WithMessage("شناسه نباید مقدار خالی داشته باشد");
+                RuleFor(e => e.Description).NotNull().NotEmpty().WithMessage("توضیحات نباید خالی باشد");
+            }
+        }
+
+        public class ContourDetailsEntityValidate : AbstractValidator<Model.ContourDetails>
+        {
+            public ContourDetailsEntityValidate()
+            {
+                RuleFor(e => e.Id).NotNull().NotEmpty().WithMessage("شناسه نباید مقدار خالی داشته باشد");
+                RuleFor(e => e.Description).NotNull().NotEmpty().WithMessage("توضیحات نباید خالی باشد");
+            }
+        }
     }
 }
