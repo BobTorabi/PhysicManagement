4dabe3d091928e962136ee295c10848354a75f59
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-03-25 20:17:17 +0430

    AddAlarmValidationAndService

--- PhysicManagement/PhysicManagement.Logic/Services/AlarmService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/AlarmService.cs
@@ -1,15 +1,134 @@
-﻿using System;
+﻿using FluentValidation;
+using PhysicManagement.Logic.Validations;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhysicManagement.Logic.Services
 {
     public class AlarmService
     {
-        // Add Alram CRUD
-        // Add AlramType CRUD
+        #region Alarm Section
+        public List<Model.Alarm> GetAlarmList()
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                return db.Alarm.OrderBy(x => x.ReviewUserName).ToList();
+            }
+        }
+        public Model.Alarm GetAlarmById(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Alarm.Find(entityId);
+                return Entity;
+            }
+        }
+        public bool AddAlarm(Model.Alarm entity)
+        {
+            var validation = new AlarmValidation.AlarmEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                db.Alarm.Add(entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+        public bool UpdateAlarm(Model.Alarm entity)
+        {
+            var validation = new AlarmValidation.AlarmEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Alarm.Find(entity.Id);
+                Entity.IsActive = entity.IsActive;
+                Entity.IsOnBoard = entity.IsOnBoard;
+                Entity.ReviewDate = entity.ReviewDate;
+                Entity.ReviewText = entity.ReviewText;
+                Entity.ReviewUserName = entity.ReviewUserName;
+                Entity.AlarmTypeId = entity.AlarmTypeId;
+                Entity.AlarmTypeTitle = entity.AlarmTypeTitle;
+                Entity.GenerateDate = entity.GenerateDate;
+                Entity.GenerateTreatmentPhaseId = entity.GenerateTreatmentPhaseId;
+                Entity.GenerateTreatmentPhaseTitle = entity.GenerateTreatmentPhaseTitle;
+                Entity.GenerateUser = entity.GenerateUser;
+
+                return db.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteAlarm(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.Alarm.Find(entityId);
+                if (Entity == null)
+                    throw new ValidationException("این رکورد در پایگاه داده وجود ندارد");
+
+                db.Alarm.Remove(Entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+        #endregion
+        #region AlarmType section
+        public List<Model.AlarmType> GetAlarmTypeList()
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                return db.AlarmType.OrderBy(x => x.Title).ToList();
+            }
+        }
+        public Model.AlarmType GetAlarmTypeById(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.AlarmType.Find(entityId);
+                return Entity;
+            }
+        }
+        public bool AddAlarmType(Model.AlarmType entity)
+        {
+            var validation = new AlarmValidation.AlarmTypeEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                db.AlarmType.Add(entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+        public bool UpdateAlarmType(Model.AlarmType entity)
+        {
+            var validation = new AlarmValidation.AlarmTypeEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.AlarmType.Find(entity.Id);
+                Entity.IsActive = entity.IsActive;
+                Entity.Title = entity.Title;
+                Entity.Description = entity.Description;
+                Entity.EnglishTitle = entity.EnglishTitle;
+
+                return db.SaveChanges() == 1;
+
+            }
+        }
+        public bool DeleteAlarmType(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.AlarmType.Find(entityId);
+                if (Entity == null)
+                    throw new ValidationException("این رکورد در پایگاه داده وجود ندارد");
+
+                db.AlarmType.Remove(Entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+
+        #endregion
 
     }
 }
--- PhysicManagement/PhysicManagement.Logic/Validations/AlarmValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/AlarmValidation.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace PhysicManagement.Logic.Validations
 {
-    class AlarmValidation
+    public class AlarmValidation
     {
+        #region AlarmTypeValidation
+        public class AlarmTypeEntityValidate : AbstractValidator<Model.AlarmType>
+        {
+            public AlarmTypeEntityValidate()
+            {
+                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
+                RuleFor(x => x.Title).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Title).Must(Common.Validate.IsPersianText).WithMessage("عنوان وارد شده صحیح نمی باشد");
+                RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Description).Must(Common.Validate.IsText).WithMessage("اطلاعات وارد شده صحیح نمی باشد");
+                RuleFor(x => x.Description).MaximumLength(200).WithMessage("متنی که نوشته اید نمی تواند بیشتر از 200 حرف باشد");
+                RuleFor(x => x.EnglishTitle).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.EnglishTitle).Must(Common.Validate.IsText).WithMessage("عنوان وارد شده صحیح نمی باشد");
+            }
+        }
+        #endregion
+        #region AlarmValidation
+        public class AlarmEntityValidate : AbstractValidator<Model.Alarm>
+        {
+            public AlarmEntityValidate()
+            {
+                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
+                RuleFor(x => x.AlarmTypeId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.AlarmTypeTitle).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.GenerateDate).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.GenerateTreatmentPhaseId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.GenerateTreatmentPhaseTitle).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.GenerateUser).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.IsOnBoard).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ReviewDate).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ReviewText).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ReviewText).Must(Common.Validate.IsText).WithMessage("اطلاعات وارد شده صحیح نمی باشد");
+                RuleFor(x => x.ReviewUserName).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+            }
+        }
+        #endregion
     }
 }
