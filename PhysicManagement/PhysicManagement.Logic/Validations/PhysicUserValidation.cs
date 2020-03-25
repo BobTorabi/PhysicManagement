7dd0f715b2c48e41c18213a7f4e419757da768f7
amir <amir.m.s.137979@gmil.com>
2020-03-25 21:15:00 +0430

    Add PhysicUserValidation

--- PhysicManagement/PhysicManagement.Logic/Validations/PhysicUserValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/PhysicUserValidation.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace PhysicManagement.Logic.Validations
 {
-    class PhysicUserValidation
-    {
-    }
+   public class PhysicUserValidation
+   {
+        public class PhysicUserEntityValidation : AbstractValidator<Model.PhysicUser>
+        {
+            public PhysicUserEntityValidation()
+            {
+                RuleFor(e => e.Id).NotNull().NotEmpty().WithMessage("شناسه نباید خالی باشد");
+                RuleFor(e => e.FirstName).NotNull().NotEmpty().WithMessage("نام نباید خالی باشد");
+                RuleFor(e => e.FirstName).Must(Common.Validate.IsPersianName).WithMessage("نام را به زبان فارسی وارد کنید");
+                RuleFor(e => e.LastName).NotNull().NotEmpty().WithMessage("نام خانوادگی نباید خالی باشد");
+                RuleFor(e => e.LastName).Must(Common.Validate.IsPersianName).WithMessage("نام خانوادگی را به زبان فارسی وارد کنید");
+                RuleFor(e => e.Username).NotNull().NotEmpty().WithMessage("نام کاربری نباید خالی باشد");
+                RuleFor(e => e.Password).NotNull().NotEmpty().WithMessage("کلمه عبور نباید خالی باشد");
+                RuleFor(e => e.Mobile).NotNull().NotEmpty().WithMessage("شماره تلفن نباید خالی باشد");
+                RuleFor(e => e.Mobile).Must(Common.Validate.IsMobile).WithMessage("شماره تلفن را به درستی وارد کنید");
+                RuleFor(e => e.Description).Must(Common.Validate.IsPersianText).WithMessage("توضیحات را به زبان فارسی وارد کنید");
+                RuleFor(e => e.Description).MaximumLength(200).WithMessage("متن نباید بیشتر از 200 حرف باشد");
+            }
+        }
+   }
 }
