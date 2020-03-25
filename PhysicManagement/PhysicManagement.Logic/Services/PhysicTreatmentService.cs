f681eb429afcba71184996686da5e2b2f6e46e79
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-03-25 12:29:32 +0430

    AddPhysicTreatmentValidationAndPhysicTreatmentService

--- PhysicManagement/PhysicManagement.Logic/Services/PhysicTreatmentService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/PhysicTreatmentService.cs
@@ -1,14 +1,71 @@
-﻿using System;
+﻿using FluentValidation;
+using PhysicManagement.Logic.Validations;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhysicManagement.Logic.Services
 {
-    class PhysicTreatmentService
+    public class PhysicTreatmentService
     {
-        // PhysicTreatment CRUD needed
+        #region PhysicTreatment Section
+        public List<Model.PhysicTreatment> GetPhysicTreatmentList()
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                return db.PhysicTreatment.OrderBy(x => x.ActionDate).ToList();
+            }
+        }
+        public Model.PhysicTreatment GetPhysicTreatmentById(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.PhysicTreatment.Find(entityId);
+                return Entity;
+            }
+        }
+        public bool AddPhysicTreatment(Model.PhysicTreatment entity)
+        {
+            var validation = new PhysicTreatmentValidation.PhysicTreatmentEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                db.PhysicTreatment.Add(entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+        public bool UpdatePhysicTreatment(Model.PhysicTreatment entity)
+        {
+            var validation = new PhysicTreatmentValidation.PhysicTreatmentEntityValidate().Validate(entity);
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.PhysicTreatment.Find(entity.Id);
+                Entity.MedicalRecordId = entity.MedicalRecordId;
+                Entity.PhaseNumber = entity.PhaseNumber;
+                Entity.PhysicTreatmentPlan = entity.PhysicTreatmentPlan;
+                Entity.ActionDate = entity.ActionDate;
+                Entity.ActionUser = entity.ActionUser;
+
+                return db.SaveChanges() == 1;
+            }
+        }
+        public bool DeletePhysicTreatment(int entityId)
+        {
+            using (var db = new Model.PhysicManagementEntities())
+            {
+                var Entity = db.PhysicTreatment.Find(entityId);
+                if (Entity == null)
+                    throw new ValidationException("این رکورد در پایگاه داده وجود ندارد");
+
+                db.PhysicTreatment.Remove(Entity);
+                return db.SaveChanges() == 1;
+            }
+        }
+        #endregion
         // PhysicTreatmentPlan CRUD needed
         // PhysicTreatmentPlanHostory CRUD needed
 
--- PhysicManagement/PhysicManagement.Logic/Validations/PhysicTreatmentValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/PhysicTreatmentValidation.cs
@@ -1,30 +1,66 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhysicManagement.Logic.Validations
 {
     public class PhysicTreatmentValidation
     {
+        #region PhysicTreatmetnValidation
         public class PhysicTreatmentEntityValidate : AbstractValidator<Model.PhysicTreatment>
         {
 
             public PhysicTreatmentEntityValidate()
             {
-                RuleFor(e => e.PhaseNumber).NotEmpty().WithMessage("");
+                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
+                RuleFor(x => x.MedicalRecordId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhaseNumber).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatmentPlan).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ActionDate).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ActionUser).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
             }
         }
+        #endregion
+        #region PhysicTreatmentPlanValidation
         public class PhysicTreatmentPlanEntityValidate : AbstractValidator<Model.PhysicTreatmentPlan>
         {
 
             public PhysicTreatmentPlanEntityValidate()
             {
-                RuleFor(e => e.Evaluation).NotEmpty().WithMessage("");
+                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
+                RuleFor(x => x.IsAcceptedByDoctor).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.IsAcceptedByPhysic).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatment).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatmentId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatmentPlanHostory).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PlannedDose).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Reserve1).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Reserve2).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.AcceptedDoctorUser).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ActionDate).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ActionUser).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.CancerOARId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Evaluation).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.HadContour).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
             }
         }
-      
+        #endregion
+        #region PhysicTreatmentPlanHOstory
+        public class PhysicTreatmentPlanHostoryEntityValidate : AbstractValidator<Model.PhysicTreatmentPlanHostory>
+        {
+            public PhysicTreatmentPlanHostoryEntityValidate()
+            {
+                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
+                RuleFor(x => x.ActionUser).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.ChangeDate).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.HasAlarm).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.IsDoctor).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Note).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatmentPlan).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.PhysicTreatmentPlanId).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+
+
+            }
+        }
+        #endregion
+
     }
 }
