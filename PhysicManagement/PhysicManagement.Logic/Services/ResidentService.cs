20bb0631d493810e086bd3b920200505a198d2bc
amir <amir.m.s.137979@gmil.com>
2020-03-25 21:41:29 +0430

    Add Some Method in ResidentServise

--- PhysicManagement/PhysicManagement.Logic/Services/ResidentService.cs
+++ PhysicManagement/PhysicManagement.Logic/Services/ResidentService.cs
@@ -8,6 +8,15 @@ namespace PhysicManagement.Logic.Services
 {
     public class ResidentService
     {
-        // Add Resident CRUD
+        public List<Model.Resident> GetResidentList()
+        { }
+        public Model.Resident GetResidentById(int entityId)
+        { }
+        public bool AddResident(Model.Resident entity)
+        { }
+        public bool UpdateResident(Model.Resident entity)
+        { }
+        public bool DeleteResident(Model.Resident entityId)
+        { }
     }
 }
