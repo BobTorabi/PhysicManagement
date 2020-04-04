2d0444f680d5d1ab443c1801064e1c13416b0b56
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 20:37:48 +0430

    Add PhysicTreatmentPlan List And Modify UI

---
+++ PhysicManagement/PhysicManagement/Controllers/PhysicTreatmentPlanController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+    public class PhysicTreatmentPlanController : Controller
+    {
+        Logic.Services.PhysicTreatmentPlanService Service;
+        public PhysicTreatmentPlanController()
+        {
+            Service = new Logic.Services.PhysicTreatmentPlanService();
+        }
+        // GET: PhysicTreatmentPlan
+        public ActionResult List()
+        {
+
+            List<Model.PhysicTreatmentPlan> PhysicTreatmentPlan = Service.GetPhysicTreatmentPlanList();
+            return View(PhysicTreatmentPlan);
+        }
+
+        public ActionResult Modify(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Model.PhysicTreatmentPlan());
+            }
+            else
+            {
+                var Entity = Service.GetPhysicTreatmentPlanById(id.GetValueOrDefault());
+                return View(Entity);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.PhysicTreatmentPlan entity)
+        {
+            return View();
+        }
+
+    }
+}
