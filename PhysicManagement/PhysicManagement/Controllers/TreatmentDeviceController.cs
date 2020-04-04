a78c1566d26b61c6a51a403cc59dd0659350b546
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 21:26:42 +0430

    Add TreatmentDevice List And Modify UI

---
+++ PhysicManagement/PhysicManagement/Controllers/TreatmentDeviceController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+    public class TreatmentDeviceController : Controller
+    {
+        Logic.Services.TreatmentService Service;
+        public TreatmentDeviceController()
+        {
+            Service = new Logic.Services.TreatmentService();
+        }
+        // GET: TreatmentDevice
+        public ActionResult List()
+        {
+
+            List<Model.TreatmentDevice> TreatmentDevice = Service.GetTreatmentDeviceList();
+            return View(TreatmentDevice);
+        }
+
+        public ActionResult Modify(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Model.TreatmentDevice());
+            }
+            else
+            {
+                var Entity = Service.GetTreatmentDeviceById(id.GetValueOrDefault());
+
+                return View(Entity);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.TreatmentDevice entity)
+        {
+            return View();
+        }
+    }
+}
