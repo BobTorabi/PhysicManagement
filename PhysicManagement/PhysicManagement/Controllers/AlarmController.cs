a9e20b75af25f666418b169c141f9f1b694efd34
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 17:12:05 +0430

    Add Alarm List UI

---
+++ PhysicManagement/PhysicManagement/Controllers/AlarmController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+    public class AlarmController : Controller
+    {
+        Logic.Services.AlarmService Service;
+        public AlarmController()
+        {
+            Service = new Logic.Services.AlarmService();
+        }
+        // GET: Alarm
+        public ActionResult List()
+        {
+            List<Model.Alarm> Alarms = Service.GetAlarmList();
+            return View(Alarms);
+        }
+    }
+}
