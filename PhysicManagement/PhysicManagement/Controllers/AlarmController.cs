dd8774177db76bc02703b24408664a92ab2404b8
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 18:49:16 +0430

    Add Alarm Modify

--- PhysicManagement/PhysicManagement/Controllers/AlarmController.cs
+++ PhysicManagement/PhysicManagement/Controllers/AlarmController.cs
@@ -19,5 +19,22 @@ namespace PhysicManagement.Controllers
             List<Model.Alarm> Alarms = Service.GetAlarmList();
             return View(Alarms);
         }
+        public ActionResult Modify(int? id)
+        {
+            if(id == null)
+            {
+                return View(new Model.Alarm());
+            }
+            else
+            {
+                var Entity = Service.GetAlarmById(id.GetValueOrDefault());
+                return View(Entity);
+            }
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.Alarm entity)
+        {
+            return View();
+        }
     }
 }
