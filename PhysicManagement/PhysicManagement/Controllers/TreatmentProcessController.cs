699b57129600d544f458fdc315a7d3353c4602f2
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 22:00:00 +0430

    Add TreatmentProcess List And Modify UI

---
+++ PhysicManagement/PhysicManagement/Controllers/TreatmentProcessController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+    public class TreatmentProcessController : Controller
+    {
+        Logic.Services.TreatmentService Service;
+        public TreatmentProcessController()
+        {
+            Service = new Logic.Services.TreatmentService();
+        }
+        // GET: TreatmentProcess
+        public ActionResult List()
+        {
+
+            List<Model.TreatmentProcess> TreatmentProcesses = Service.GetTreatmentProcessList();
+            return View(TreatmentProcesses);
+        }
+
+        public ActionResult Modify(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Model.TreatmentProcess());
+            }
+            else
+            {
+                var Entity = Service.GetTreatmentProcessById(id.GetValueOrDefault());
+                return View(Entity);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.TreatmentProcess entity)
+        {
+            return View();
+        }
+    }
+}
