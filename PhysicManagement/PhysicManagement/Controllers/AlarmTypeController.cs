68df2a7e24c732a15dc6fc71037f8ecce93aad16
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-04 19:08:30 +0430

    Add AlarmType List and Modify UI

---
+++ PhysicManagement/PhysicManagement/Controllers/AlarmTypeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+    public class AlarmTypeController : Controller
+    {
+        Logic.Services.AlarmService Service;
+        public AlarmTypeController()
+        {
+            Service = new Logic.Services.AlarmService();
+        }
+        // GET: AlarmType
+        public ActionResult List()
+        {
+
+            List<Model.AlarmType> AlarmTypes = Service.GetAlarmTypeList();
+            return View(AlarmTypes);
+        }
+
+        public ActionResult Modify(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Model.AlarmType());
+            }
+            else
+            {
+                var Entity = Service.GetAlarmTypeById(id.GetValueOrDefault());
+                return View(Entity);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Modify(Model.AlarmType entity)
+        {
+            return View();
+        }
+
+
+    }
+}
