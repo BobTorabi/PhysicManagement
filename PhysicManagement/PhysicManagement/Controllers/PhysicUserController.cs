72c86113c01fe1aec4e08ee8759ebfefab85ea77
amir <amir.m.s.137979@gmil.com>
2020-04-09 03:25:01 +0430

    Update PhysicUserController

---
+++ PhysicManagement/PhysicManagement/Controllers/PhysicUserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhysicManagement.Controllers
+{
+
+
+
+    public class PhysicUserController : BaseController
+    {
+        Logic.Services.PhysicUserService Service;
+        public PhysicUserController()
+        {
+            Service = new Logic.Services.PhysicUserService();
+        }
+        // GET: Doctor
+        public ActionResult List()
+        {
+
+            List<Model.PhysicUser> PhysicUser = Service.GetPhysicUserList();
+            return View(PhysicUser);
+        }
+
+        public ActionResult Modify(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Model.PhysicUser());
+            }
+            else
+            {
+                var Entity = Service.GetPhysicUserById(id.GetValueOrDefault());
+                Entity.Password = Logic.Services.PhysicUserService.DecryptPassword(Entity.Username, Entity.Password);
+                return View(Entity);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.PhysicUser entity)
+        {
+            bool IsAffected;
+            if (entity.Id > 0)
+            {
+                IsAffected = Service.UpdatePhysicUser(entity);
+            }
+            else
+            {
+                IsAffected = Service.AddPhysicUser(entity);
+            }
+            if (IsAffected)
+                return Redirect("Index");
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
