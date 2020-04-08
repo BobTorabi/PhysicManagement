b36110df737ec62fd6bf6e494db46f98d5543437
amirabbas sepanlou <amirsepanlou@yahoo.com>
2020-04-09 01:09:19 +0430

    Update AlarmType Validator

--- PhysicManagement/PhysicManagement.Logic/Validations/AlarmValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/AlarmValidation.cs
@@ -9,15 +9,16 @@ namespace PhysicManagement.Logic.Validations
         {
             public AlarmTypeEntityValidate()
             {
-                RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد ");
-                RuleFor(x => x.Title).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Title).Null().WithMessage("عنوان نمی تواند نال باشد")
+                    .Empty().WithMessage("پر کردن عنوان اجباری است ");
                 RuleFor(x => x.Title).Must(Common.Validate.IsPersianText).WithMessage("عنوان وارد شده صحیح نمی باشد");
-                RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
-                RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
+                RuleFor(x => x.Description).Null().WithMessage("توضیحات نمی تواند نال باشد")
+                    .Empty().WithMessage("پر کردن توضیحات اجباری است ");
                 RuleFor(x => x.Description).Must(Common.Validate.IsText).WithMessage("اطلاعات وارد شده صحیح نمی باشد");
                 RuleFor(x => x.Description).MaximumLength(200).WithMessage("متنی که نوشته اید نمی تواند بیشتر از 200 حرف باشد");
-                RuleFor(x => x.EnglishTitle).NotNull().NotEmpty().WithMessage("پر کردن این فیلد اجباری است ");
-                RuleFor(x => x.EnglishTitle).Must(Common.Validate.IsText).WithMessage("عنوان وارد شده صحیح نمی باشد");
+                RuleFor(x => x.EnglishTitle).Null().WithMessage("عنوان انگلیسی نمی تواند نال باشد")
+                    .Empty().WithMessage("پر کردن عنوان انگلیسی اجباری است ");
+                RuleFor(x => x.EnglishTitle).Must(Common.Validate.IsText).WithMessage("عنوان انگلیسی وارد شده صحیح نمی باشد");
             }
         }
         #endregion
