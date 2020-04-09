1218852e894292b5f6c77c92eb3823612287dd06
amir <amir.m.s.137979@gmil.com>
2020-04-09 17:35:01 +0430

    Update DoctorValidation

--- PhysicManagement/PhysicManagement.Logic/Validations/DoctorValidation.cs
+++ PhysicManagement/PhysicManagement.Logic/Validations/DoctorValidation.cs
@@ -14,13 +14,12 @@ namespace PhysicManagement.Logic.Validations
             public DoctorEntityValidate()
             {
                 RuleFor(e => e.Id).NotNull().WithMessage("شناسه نمی تواند مقدار خالی داشته باشد");
-                RuleFor(x => x.FirstName).NotNull().NotEmpty().WithMessage("نام نمی تواند مقدار خالی داشته باشد");
+                RuleFor(x => x.FirstName).Length(1 , 150).WithMessage("نام  1 تا 150 حرف داشته باشد");
                 RuleFor(e => e.FirstName).Must(Common.Validate.IsPersianName).WithMessage("نام را به زبان فارسی وارد کنید");
                 RuleFor(x => x.Username).NotNull().NotEmpty().WithMessage("نام کاربری نمی تواند مقدار خالی داشته باشد");
                 RuleFor(x => x.Password).NotNull().NotEmpty().WithMessage("رمز عبور نمی تواند مقدار خالی داشته باشد");
-                RuleFor(x => x.LastName).NotNull().NotEmpty().WithMessage("نام خانوادگی نمی تواند مقدار خالی داشته باشد");
+                RuleFor(x => x.LastName).Length(1 ,150).WithMessage("نام خانوادگی باید 1 تا 150 حرف داشته باشد");
                 RuleFor(e => e.LastName).Must(Common.Validate.IsPersianName).WithMessage("نام خانوادگی را به زبان فارسی وارد کنید");
-                RuleFor(x => x.Code).NotNull().NotEmpty().WithMessage("کد نمی تواند مقدار خالی داشته باشد");
             }
         }
     }
