16c059b31f24f0aa37aca90e8bba30f51e9bd65b
amir <amir.m.s.137979@gmil.com>
2020-04-11 14:11:52 +0430

    Add CancerOARController

---
+++ PhysicManagement/PhysicManagement/Controllers/CancerOARController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhysicManagement.Logic.Services;
+
+namespace PhysicManagement.Controllers
+{
+    public class CancerOARController : BaseController
+    {
+        Logic.Services.CancerService Service;
+        public CancerOARController()
+        {
+            Service = new Logic.Services.CancerService();
+        }
+        public ActionResult Index()
+        {
+            return RedirectToActionPermanent("List");
+        }
+        // GET: CancerOAR
+        public ActionResult List()
+        {
+            List<Model.CancerOAR> CancerOAR = Service.GetCancerOARList();
+            return View(CancerOAR);
+        }
+        public ActionResult Modify(int? id)
+        {
+            
+            if (id == null)
+            {
+                return View(new Model.CancerOAR());
+            }
+            else
+            {
+                var Entity = Service.GetCancerOARById(id.GetValueOrDefault());
+                return View(Entity);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Modify(Model.CancerOAR entity)
+        {
+            bool IsAffected;
+            if (entity.Id > 0)
+            {
+                IsAffected = Service.UpdateCancerOAR(entity);
+            }
+            else
+            {
+                IsAffected = Service.AddCancerOAR(entity);
+            }
+            if (IsAffected)
+                return RedirectToAction("Index");
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
