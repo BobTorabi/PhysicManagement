149063882023bd0bfee12c628555185af575d669
Mohyaf <mohyaf@yahoo.com>
2020-06-22 02:53:17 +0430

    Form 8 added

---
+++ PhysicManagement/PhysicManagement/Models/SetPhaseDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhysicManagement.Models
+{
+    public class SetPhaseDetail
+    {
+        public string PhaseId { get; set; }
+        public string PhaseDetailId { get; set; }
+        public string Evaluation { get; set; }
+        public string PlannedDose { get; set; }
+        public string CancerOARId { get; set; }
+    }
+
+}
